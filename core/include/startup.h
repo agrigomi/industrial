853befcfc872accef8abef622025effe13854d5e
Mihail Grigorov <michael.grigorov@konsulko.com>
2018-01-17 17:46:47 +0200

    Add state flags to base entry
    Signed-off-by: Mihail Grigorov <michael.grigorov@konsulko.com>

--- core/include/startup.h
+++ core/include/startup.h
@@ -7,9 +7,13 @@
 #define _EXPORT_ __attribute__ ((visibility ("default")))
 #define _LOCAL_ __attribute__ ((visibility ("hidden")))
 
+#define ST_INITIALIZED	(1<<0)
+#define ST_DISABLED	(1<<1)
+
 typedef struct {
 	iBase *pi_base;
 	_u32  ref_cnt;
+	_u8   state;
 }_base_entry_t;
 
 typedef std::vector<_base_entry_t> _base_vector_t;
