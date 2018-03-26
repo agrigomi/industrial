78cb94886e546f54841c3bdbb51fe322274c72bd
agrigomi <m_e_grigorov@abv.bg>
2018-03-26 03:18:06 +0300

    net: implementation (initial commit)
    Signed-off-by: agrigomi <m_e_grigorov@abv.bg>

---
+++ io/libnet/net.cpp
@@ -0,0 +1,48 @@
+#include "startup.h"
+#include "iNet.h"
+
+IMPLEMENT_BASE_ARRAY(10)
+
+class cNet: public iNet {
+public:
+	BASE(cNet, "cNet", RF_ORIGINAL, 1,0,0);
+
+	bool object_ctl(_u32 cmd, void *arg, ...) {
+		bool r = false;
+
+		switch(cmd) {
+			case OCTL_INIT:
+				break;
+			case OCTL_UNINIT:
+				break;
+		}
+
+		return r;
+	}
+	
+	iUDPServer *create_udp_server(_u32 port) {
+		iUDPServer *r = 0;
+		//...
+		return r;
+	}
+
+	iUDPClient *create_udp_client(_str_t dst_ip, _u32 port) {
+		iUDPClient *r = 0;
+		//...
+		return r;
+	}
+
+	iTCPServer *create_tcp_server(_u32 port) {
+		iTCPServer *r = 0;
+		//...
+		return r;
+	}
+
+	iTCPClient *create_tcp_client(_str_t dst_ip, _u32 port) {
+		iTCPClient *r = 0;
+		//...
+		return r;
+	}
+};
+
+static cNet _g_net_;
