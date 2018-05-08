828489f994dc4c22608ea589867dec6582b425fc
agrigomi <m_e_grigorov@abv.bg>
2018-05-08 19:10:56 +0300

    cmd: interface update
    Signed-off-by: agrigomi <m_e_grigorov@abv.bg>

--- cmd/interface/iCmd.h
+++ cmd/interface/iCmd.h
@@ -3,11 +3,45 @@
 
 #include "iIO.h"
 
-#define I_CMD	"iCmd"
+#define I_CMD		"iCmd"
+#define I_CMD_HOST	"iCmdHost"
+
+// option flags
+#define OF_PRESENCE	(1<<0)	// for presence in command line
+#define OF_VALUE	(1<<1)	// value required
+#define OF_LONG		(1<<3)	// flag for long option (lead by --)
+
+typedef struct {
+	_cstr_t	opt_name;
+	_u32	opt_flags;
+	_str_t	opt_value;
+	_cstr_t	opt_help;
+}_cmd_opt_t;
+
+class iCmd;
+typedef void _cmd_handler_t(iCmd *, // command opbect
+			_cmd_opt_t *, // options array
+			iIO *, // I/O object
+			_str_t args[], // additional arguments
+			_u8 nargs // number of additional arguments
+			);
+
+typedef struct {
+	_cstr_t 	cmd_name;
+	_cmd_opt_t	*cmd_options;
+	_cmd_handler_t	*cmd_handler;
+}_cmd_t;
 
 class iCmd: public iBase {
 public:
 	INTERFACE(iCmd, I_CMD);
+	virtual _cmd_t *get_info(void)=0;
+};
+
+class iCmdHost: public iBase {
+public:
+	INTERFACE(iCmdHost, I_CMD_HOST);
+	virtual void exec(_str_t, iIO *)=0;
 };
 
 #endif
--- cmd/libcmd/cmd.cpp
+++ cmd/libcmd/cmd.cpp
@@ -1,26 +1,37 @@
 #include "iCmd.h"
+#include "iStr.h"
+#include "iRepository.h"
 #include "startup.h"
 
 IMPLEMENT_BASE_ARRAY("libcmd", 10)
 
-class cCmd: public iCmd {
+class cCmdHost: public iCmdHost {
 private:
+	iStr	*mpi_str;
 public:
-	BASE(cCmd, "cCmd", RF_ORIGINAL, 1,0,0);
+	BASE(cCmdHost, "cCmdHost", RF_ORIGINAL, 1,0,0);
 
 	bool object_ctl(_u32 cmd, void *arg, ...) {
 		bool r = false;
 
 		switch(cmd) {
 			case OCTL_INIT:
-				r = true;
+				if((mpi_str = (iStr *)_gpi_repo_->object_by_iname(I_STR, RF_ORIGINAL)))
+					r = true;
 				break;
 			case OCTL_UNINIT:
+				if(mpi_str)
+					_gpi_repo_->object_release(mpi_str);
 				r = true;
 				break;
 		}
 
 		return r;
 	}
+
+	void exec(_str_t cmd_line, iIO *pi_io) {
+		//...
+	}
 };
 
+static cCmdHost _g_cmd_host_;
