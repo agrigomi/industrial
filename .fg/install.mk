e23cf9d6ab30a3496ba7eacecfc048f500c4168a
agrigomi <m_e_grigorov@abv.bg>
2018-06-03 01:47:10 +0300

    build: update of install script
    Signed-off-by: agrigomi <m_e_grigorov@abv.bg>

--- .fg/install.mk
+++ .fg/install.mk
@@ -1,4 +1,29 @@
+include .fg/_config_
+include .fg/$(PROJECT)/_config_
+targets = $(shell cat .fg/$(PROJECT)/$(CONFIG)/_targets_)
+targets_basename = $(basename $(targets))
+
+#default output directory
+OUTDIR=$(DEFAULT_OUTDIR)
+
+ifeq ($(CONFIG),)
+	CONFIG=$(DEFAULT_CONFIG)
+endif
+
+ifeq ($(CONFIG),)
+	exit
+endif
+
+-include .fg/$(PROJECT)/$(CONFIG)/_config_
+
+.PHONY: $(targets_basename) clean
+
+$(targets_basename):
+	for i in $(targets); do \
+		echo ' ' install target [$$i]; \
+	done
+
 .PHONY: $(PROJECT)
 
-$(PROJECT):
-	@echo ---
+$(PROJECT): $(targets_basename)
+	@echo install $@
