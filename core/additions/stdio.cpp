6f6ed1a5a23bf39075de4bde093448377a87fa96
Mihail Grigorov <megrigorov@gmail.com>
2018-06-17 20:55:55 +0300

    stdio: interface and implementation
    Signed-off-by: Mihail Grigorov <megrigorov@gmail.com>

---
+++ core/additions/stdio.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "iIO.h"
+
+class cStdIO: public iStdIO {
+private:
+public:
+	BASE(cStdIO, "cStdIO", RF_ORIGINAL, 1,0,0);
+
+	bool object_ctl(_u32 cmd, void *arg, ...) {
+		bool r = false;
+
+		switch(cmd) {
+			case OCTL_INIT:
+			case OCTL_UNINIT:
+				r = true;
+		}
+
+		return r;
+	}
+
+	_u32 read(void *data, _u32 size) {
+		_u32 r = 0;
+
+		if(fread(data, size, 1, stdin) == 1)
+			r = size;
+
+		return r;
+	}
+
+	_u32 write(void *data, _u32 size) {
+		_u32 r = 0;
+
+		if(::fwrite(data, size, 1, stdout) == 1)
+			r = size;
+
+		return r;
+	}
+
+	_u32 fwrite(_cstr_t fmt, ...) {
+		_u32 r = 0;
+		va_list va;
+
+		va_start(va, fmt);
+		r = vfprintf(stdout, fmt, va);
+		va_end(va);
+
+		return r;
+	}
+};
+
+static cStdIO _g_stdio_;
--- core/interface/iIO.h
+++ core/interface/iIO.h
@@ -4,12 +4,19 @@
 #include "iBase.h"
 
 #define I_IO		"iIO"
+#define I_STD_IO	"iStdIO"
 
 class iIO: public iBase {
 public:
 	INTERFACE(iIO, I_IO);
 	virtual _u32 read(void *data, _u32 size)=0;
 	virtual _u32 write(void *data, _u32 size)=0;
+};
+
+class iStdIO: public iIO {
+public:
+	INTERFACE(iStdIO, I_STD_IO);
+	virtual _u32 fwrite(_cstr_t fmt, ...)=0;
 };
 
 #endif
