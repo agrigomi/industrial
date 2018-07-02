7ee4adef7b69d5a3ecd9e9227ca92341f864f0b7
Mihail Grigorov <megrigorov@gmail.com>
2018-07-02 15:15:22 +0300

    hypertext: xml: Adds function for creation of XML context
    Signed-off-by: Mihail Grigorov <megrigorov@gmail.com>

--- hypertext/libht/xml.c
+++ hypertext/libht/xml.c
@@ -1,3 +1,23 @@
+#include <string.h>
 #include "context.h"
 #include "xml.h"
+
+/* allocate memory for XML context */
+_xml_context_t *xml_create_context(_mem_alloc_t *p_malloc, _mem_free_t *p_free) {
+	_xml_context_t *r = NULL;
+
+	if(p_malloc && p_free) {
+		if((r = p_malloc(sizeof(_xml_context_t)))) {
+			memset(r, 0, sizeof(_xml_context_t));
+
+			/* create hypertext context */
+			if(!(r->p_htc = ht_create_context(p_malloc, p_free))) {
+				p_free(r, sizeof(_xml_context_t));
+				r = NULL;
+			}
+		}
+	}
+
+	return r;
+}
 
