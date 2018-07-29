175feeacc1f74fcfd3500a793277eaf03647da27
Mihail Grigorov <agrigomi@abv.bg>
2018-07-29 14:01:21 +0300

    HT: interface comments
    * shell script for project clean
      usage: ./pclean <project name>
    * code update in core/test
    
    Signed-off-by: Mihail Grigorov <agrigomi@abv.bg>

--- hypertext/interface/iHT.h
+++ hypertext/interface/iHT.h
@@ -12,12 +12,31 @@
 class iHT:public iBase {
 public:
 	INTERFACE(iHT, I_HT);
+	// Create parser context
 	virtual HTCONTEXT create_context(void)=0;
+	// destroy parser context
 	virtual void destroy_context(HTCONTEXT)=0;
-	virtual bool parse(HTCONTEXT, _str_t, _ulong)=0;
-	virtual HTTAG select(HTCONTEXT, _cstr_t xpath, HTTAG, _u32)=0;
-	virtual _str_t parameter(HTCONTEXT, HTTAG, _cstr_t, _u32 *)=0;
-	virtual _str_t content(HTTAG, _u32 *)=0;
+	// Parse (indexing) hypertext content
+	virtual bool parse(HTCONTEXT, // parser context
+			_str_t, // Content of hypertext
+			_ulong // Size of hypertext content
+			)=0;
+	// Select hypertext tag
+	virtual HTTAG select(HTCONTEXT, // parser context
+			_cstr_t xpath, // path to tag (or NULL for inddex only enumeration)
+			HTTAG, // start point (or NULL for root tag)
+			_u32 // index
+			)=0;
+	// Get parameter value
+	virtual _str_t parameter(HTCONTEXT, // parser context
+			HTTAG, // tag handle (by select)
+			_cstr_t, // parameter name
+			_u32 * // [Out] Value size in symbols (not bytes)
+			)=0;
+	// Get tag content
+	virtual _str_t content(HTTAG, // tag handle (by select)
+			_u32 * // [Out] content size in symbols (not bytes)
+			)=0;
 };
 
 class iXML: public iHT {
