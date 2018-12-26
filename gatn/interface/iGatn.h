925c50db30d6c116eff88d9f97988ec36a316193
Mihail Grigorov <agrigomi@abv.bg>
2018-12-26 12:11:25 +0200

    gatn: Initial commit of 'gatn' web framework
    Signed-off-by: Mihail Grigorov <agrigomi@abv.bg>

---
+++ gatn/interface/iGatn.h
@@ -0,0 +1,9 @@
+#include "iNet.h"
+#include "iFS.h"
+
+#define I_GATN	"iGatn"
+
+class iGatn: public iBase {
+public:
+	INTERFACE(iGatn, I_GATN);
+};
---
+++ gatn/libgatn/gatn.cpp
@@ -0,0 +1,5 @@
+#include "startup.h"
+#include "iGatn.h"
+
+IMPLEMENT_BASE_ARRAY("libgatn", 10);
+
