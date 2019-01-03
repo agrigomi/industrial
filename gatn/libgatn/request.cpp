9c9511ac097e0536f997c09e006e8f2dd4990dd8
Mihail Grigorov <agrigomi@abv.bg>
2019-01-03 21:30:08 +0200

    gatn: Code update (implementation of request/response objects)
    Signed-off-by: Mihail Grigorov <agrigomi@abv.bg>

--- gatn/libgatn/request.cpp
+++ gatn/libgatn/request.cpp
@@ -1,4 +1,57 @@
 #include "iGatn.h"
 #include "private.h"
 
+_cstr_t request::header(void) {
+	_cstr_t r = NULL;
+
+	if(mpi_httpc)
+		r = mpi_httpc->req_header();
+
+	return r;
+}
+
+_cstr_t request::utl(void) {
+	_cstr_t r = NULL;
+
+	if(mpi_httpc)
+		r = mpi_httpc->req_url();
+
+	return r;
+}
+
+_cstr_t request::urn(void) {
+	_cstr_t r = NULL;
+
+	if(mpi_httpc)
+		r = mpi_httpc->req_urn();
+
+	return r;
+}
+
+_cstr_t request::var(_cstr_t name) {
+	_cstr_t r = NULL;
+
+	if(mpi_httpc)
+		r = mpi_httpc->req_var(name);
+
+	return r;
+}
+
+_u32 request::content_len(void) {
+	_u32 r = 0;
+
+	if(mpi_httpc)
+		r = mpi_httpc->req_content_len();
+
+	return r;
+}
+
+void *request::data(_u32 *size) {
+	void *r = NULL;
+
+	if(mpi_httpc)
+		r = mpi_httpc->req_data(size);
+
+	return r;
+}
 
--- gatn/libgatn/response.cpp
+++ gatn/libgatn/response.cpp
@@ -1,3 +1,23 @@
 #include "iGatn.h"
 #include "private.h"
 
+void response::var(_cstr_t name, _cstr_t value) {
+	if(mpi_httpc)
+		mpi_httpc->res_var(name, value);
+}
+
+_u32 response::write(void *data, _u32 size) {
+	_u32 r = 0;
+
+	//...
+
+	return r;
+}
+
+_u32 response::end(_u16 response_code, void *data, _u32 size) {
+	_u32 r = 0;
+
+	//...
+
+	return r;
+}
