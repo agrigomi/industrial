088cee63c3266a871833b7683f1b4f3037f19e79
Mihail Grigorov <agrigomi@abv.bg>
2019-07-25 22:52:29 +0300

    core: code update (tArray & tChArray)
    Signed-off-by: Mihail Grigorov <agrigomi@abv.bg>

--- core/include/array.h
+++ core/include/array.h
@@ -6,6 +6,8 @@
 #include "iMemory.h"
 #include "iRepository.h"
 
+#define INITIAL_ARRAY_SIZE 16
+
 template <class T>
 class _LOCAL_ tArray {
 private:
@@ -15,6 +17,7 @@ private:
 	_u32 	m_size;
 	iHeap 	*mpi_heap;
 	bool	my_heap;
+	bool	m_is_init;
 
 	bool realloc(void) {
 		bool r = false;
@@ -42,21 +45,38 @@ private:
 	}
 
 public:
-	tArray(int capacity, iHeap *pi_heap=NULL) {
-		m_capacity = 0;
-		mp_array = NULL;
-		m_size = 0;
-		m_initial = capacity;
-		my_heap = false;
+	tArray(_u32 capacity, iHeap *pi_heap=NULL) {
+		m_is_init = false;
+		init(capacity, pi_heap);
+	}
 
-		if(!(mpi_heap = pi_heap)) {
-			mpi_heap = (iHeap *)_gpi_repo_->object_by_iname(I_HEAP, RF_ORIGINAL);
-			my_heap = true;
-		}
+	tArray() {
+		m_capacity = m_size = 0;
+		m_initial = INITIAL_ARRAY_SIZE;
+		mpi_heap = NULL;
+		mp_array = NULL;
+		my_heap = m_is_init = false;
 	}
 
 	~tArray() {
 		destroy();
+	}
+
+	void init(_u32 capacity, iHeap *pi_heap) {
+		if(!m_is_init) {
+			m_capacity = 0;
+			mp_array = NULL;
+			m_size = 0;
+			m_initial = capacity;
+			my_heap = false;
+
+			if(!(mpi_heap = pi_heap)) {
+				mpi_heap = (iHeap *)_gpi_repo_->object_by_iname(I_HEAP, RF_ORIGINAL);
+				my_heap = true;
+			}
+
+			m_is_init = true;
+		}
 	}
 
 	T& get(_u32 i) {
@@ -105,9 +125,63 @@ public:
 			mpi_heap->free((void *)mp_array, m_capacity * sizeof(T));
 			mp_array = NULL;
 			m_capacity = m_size = m_initial = 0;
-			if(my_heap)
+			if(my_heap) {
 				_gpi_repo_->object_release(mpi_heap);
+				mpi_heap = NULL;
+			}
+
+			m_is_init = false;
 		}
+	}
+};
+
+#define INITIAL_CH_ARRAY	16
+
+template <class _T>
+class tChArray { //Chunked array
+private:
+	tArray<_T*> m_array;
+	_u32 	m_capacity;
+	_u32 	m_size;
+	_u32	m_initial;
+	iHeap 	*mpi_heap;
+	bool	my_heap;
+	bool	m_is_init;
+public:
+
+	tChArray(_u32 capacity, iHeap *pi_heap=NULL) {
+		m_is_init = false;
+		init(capacity, pi_heap);
+	}
+
+	tChArray() {
+		m_capacity = m_size = m_initial = 0;
+		mpi_heap = NULL;
+		my_heap = m_is_init = false;
+	}
+
+	~tChArray() {
+		destroy();
+	}
+
+	void init(_u32 capacity, iHeap *pi_heap) {
+		if(!m_is_init) {
+			my_heap = false;
+			m_initial = capacity;
+			m_capacity = 0;
+			m_size = 0;
+
+			if(!(mpi_heap = pi_heap)) {
+				mpi_heap = (iHeap *)_gpi_repo_->object_by_iname(I_HEAP, RF_ORIGINAL);
+				my_heap = true;
+			}
+
+			m_array.init(INITIAL_CH_ARRAY, mpi_heap);
+			m_is_init = true;
+		}
+	}
+
+	void destroy(void) {
 	}
 };
 
