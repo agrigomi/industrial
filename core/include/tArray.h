2e7c249111aa4e22c140be60ed3eaff3633f65d9
Mihail Grigorov <megrigorov@gmail.com>
2019-07-26 11:58:26 +0300

    core: Adds tArray & tVector templates
    Signed-off-by: Mihail Grigorov <megrigorov@gmail.com>

---
+++ core/include/tArray.h
@@ -0,0 +1,170 @@
+#ifndef __TARRAY_H__
+#define __TARRAY_H__
+
+#include <string.h>
+#include <mutex>
+#include "startup.h"
+#include "iMemory.h"
+#include "iRepository.h"
+
+#define INITIAL_ARRAY_SIZE 16
+
+template <class T>
+class _LOCAL_ tArray {
+private:
+	T 		*mp_array;
+	_u32 		m_capacity;
+	_u32		m_initial;
+	_u32 		m_size;
+	iHeap 		*mpi_heap;
+	bool		my_heap;
+	bool		m_is_init;
+	std::mutex	m_mutex;
+
+	iHeap *get_heap(void) {
+		iHeap *r = mpi_heap;
+
+		if(!r) {
+			r = mpi_heap = (iHeap *)_gpi_repo_->object_by_iname(I_HEAP, RF_ORIGINAL);
+			my_heap = true;
+		}
+
+		return r;
+	}
+
+	bool realloc(void) {
+		bool r = false;
+		T *_old = mp_array;
+		_u32 sz_new = (m_capacity + m_initial) * sizeof(T);
+		_u32 sz_old = m_capacity * sizeof(T);
+		iHeap *pi_heap = get_heap();
+
+		if(pi_heap) {
+			T *_new = (T *)pi_heap->alloc(sz_new);
+
+			if(_new) {
+				memset((void *)_new, 0, sz_new);
+				if(_old) {
+					memcpy((void *)_new, (void *)_old, sz_old);
+					mpi_heap->free((void *)_old, sz_old);
+				}
+
+				mp_array = _new;
+				m_capacity += m_initial;
+				r = true;
+			}
+		}
+
+		return r;
+	}
+
+public:
+	tArray(_u32 capacity, iHeap *pi_heap=NULL) {
+		m_is_init = false;
+		init(capacity, pi_heap);
+	}
+
+	tArray() {
+		m_capacity = m_size = 0;
+		m_initial = INITIAL_ARRAY_SIZE;
+		mpi_heap = NULL;
+		mp_array = NULL;
+		my_heap = m_is_init = false;
+	}
+
+	~tArray() {
+		destroy();
+	}
+
+	void init(_u32 capacity, iHeap *pi_heap) {
+		if(!m_is_init) {
+			m_capacity = 0;
+			mp_array = NULL;
+			m_size = 0;
+			m_initial = capacity;
+			my_heap = false;
+
+			if(!(mpi_heap = pi_heap))
+				get_heap();
+
+			m_is_init = true;
+		}
+	}
+
+	T& get(_u32 i) {
+		T *r = NULL;
+
+		m_mutex.lock();
+		if(i < m_size)
+			r = &mp_array[i];
+		m_mutex.unlock();
+
+		return *r;
+	}
+
+	T& operator [](_u32 i) {
+		return get(i);
+	}
+
+	void add(const T &item) {
+		bool a = true;
+
+		m_mutex.lock();
+		if(m_size == m_capacity)
+			a = realloc();
+
+		if(a) {
+			mp_array[m_size] = item;
+			m_size++;
+		}
+		m_mutex.unlock();
+	}
+
+	void operator +=(const T &item) {
+		add(item);
+	}
+
+	_u32 size(void) {
+		_u32 r = 0;
+
+		m_mutex.lock();
+		r = m_size;
+		m_mutex.unlock();
+		return r;
+	}
+
+	void clean(void) {
+		m_mutex.lock();
+		if(mp_array && m_capacity) {
+			iHeap *pi_heap = get_heap();
+
+			if(pi_heap)
+				pi_heap->free((void *)mp_array, m_capacity * sizeof(T));
+
+			mp_array = NULL;
+			m_capacity = m_size = 0;
+		}
+		m_mutex.unlock();
+	}
+
+	void destroy(void) {
+		m_mutex.lock();
+		if(mp_array && m_capacity) {
+			iHeap *pi_heap = get_heap();
+
+			if(pi_heap)
+				pi_heap->free((void *)mp_array, m_capacity * sizeof(T));
+
+			mp_array = NULL;
+			m_capacity = m_size = 0;
+			m_initial = INITIAL_ARRAY_SIZE;
+			if(my_heap)
+				_gpi_repo_->object_release(mpi_heap);
+			mpi_heap = NULL;
+			m_is_init = my_heap = false;
+		}
+		m_mutex.unlock();
+	}
+};
+
+#endif
---
+++ core/include/tVector.h
@@ -0,0 +1,188 @@
+#ifndef __TVECTOR_H__
+#define __TVECTOR_H__
+
+
+#include <string.h>
+#include "startup.h"
+#include "iMemory.h"
+#include "iRepository.h"
+#include "tArray.h"
+
+#define INITIAL_CH_ARRAY	4
+
+template <class _T>
+class tVector { //Chunked array
+private:
+	tArray<_T*> 	m_array;
+	_u32 		m_capacity;
+	_u32 		m_size;
+	_u32		m_initial;
+	iHeap 		*mpi_heap;
+	bool		my_heap;
+	bool		m_is_init;
+	std::mutex	m_mutex;
+
+	iHeap *get_heap(void) {
+		iHeap *r = mpi_heap;
+
+		if(!r) {
+			r = mpi_heap = (iHeap *)_gpi_repo_->object_by_iname(I_HEAP, RF_ORIGINAL);
+			my_heap = true;
+		}
+
+		return r;
+	}
+
+	bool alloc_chunk(void) {
+		bool r = false;
+		iHeap *pi_heap = get_heap();
+
+		if(pi_heap) {
+			_T *p = (_T *)pi_heap->alloc(m_initial * sizeof(_T));
+
+			if(p) {
+				m_array += p;
+				m_capacity += m_initial;
+				r = true;
+			}
+		}
+
+		return r;
+	}
+
+public:
+
+	tVector(_u32 capacity, iHeap *pi_heap=NULL) {
+		m_is_init = false;
+		init(capacity, pi_heap);
+	}
+
+	tVector() {
+		m_capacity = m_size = 0;
+		m_initial = INITIAL_ARRAY_SIZE;
+		mpi_heap = NULL;
+		my_heap = m_is_init = false;
+	}
+
+	~tVector() {
+		destroy();
+	}
+
+	void init(_u32 capacity, iHeap *pi_heap) {
+		if(!m_is_init) {
+			my_heap = false;
+			m_initial = capacity;
+			m_capacity = 0;
+			m_size = 0;
+
+			if(!(mpi_heap = pi_heap))
+				get_heap();
+
+			m_array.init(INITIAL_CH_ARRAY, mpi_heap);
+			m_is_init = true;
+		}
+	}
+
+	_T& get(_u32 i) {
+		_T *r = NULL;
+
+		m_mutex.lock();
+		if(i < m_size) {
+			_u32 chunk = i / m_initial;
+			_u32 idx = i % m_initial;
+			_T *p = m_array[chunk];
+
+			if(p)
+				r = &p[idx];
+		}
+		m_mutex.unlock();
+
+		return *r;
+	}
+
+	_T& operator [](_u32 i) {
+		return get(i);
+	}
+
+	void add(const _T &item) {
+		bool a = true;
+
+		m_mutex.lock();
+		if(m_size == m_capacity)
+			a = alloc_chunk();
+
+		if(a) {
+			_u32 chunk = m_size / m_initial;
+			_u32 idx = m_size % m_initial;
+			_T *p = m_array[chunk];
+
+			p[idx] = item;
+			m_size++;
+		}
+		m_mutex.unlock();
+	}
+
+	void operator +=(const _T &item) {
+		add(item);
+	}
+
+	_u32 size(void) {
+		_u32 r = 0;
+
+		m_mutex.lock();
+		r = m_size;
+		m_mutex.unlock();
+		return r;
+	}
+
+	void clean(void) {
+		m_mutex.lock();
+		if(m_is_init) {
+			iHeap *pi_heap = get_heap();
+			_u32 sz = m_array.size();
+
+			if(pi_heap) {
+				for(_u32 i = 0; i  < sz; i++) {
+					_T *ptr = m_array[i];
+
+					if(ptr)
+						mpi_heap->free(ptr, m_initial * sizeof(_T));
+				}
+			}
+
+			m_array.clean();
+			m_capacity = m_size = 0;
+		}
+		m_mutex.unlock();
+	}
+
+	void destroy(void) {
+		m_mutex.lock();
+		if(m_is_init) {
+			_u32 sz = m_array.size();
+			iHeap *pi_heap = get_heap();
+
+			if(pi_heap) {
+				for(_u32 i = 0; i  < sz; i++) {
+					_T *ptr = m_array[i];
+
+					if(ptr)
+						pi_heap->free(ptr, m_initial * sizeof(_T));
+				}
+			}
+
+			m_array.destroy();
+			m_capacity = m_size = 0;
+			m_initial = INITIAL_ARRAY_SIZE;
+
+			if(my_heap)
+				_gpi_repo_->object_release(mpi_heap);
+
+			mpi_heap = NULL;
+			m_is_init = my_heap = false;
+		}
+		m_mutex.unlock();
+	}
+};
+
+#endif
