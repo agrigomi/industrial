9af284e3139f3b811f59ab8b63e55f4fe197f25f
Mihail Grigorov <megrigorov@gmail.com>
2019-09-10 12:22:13 +0300

    core: Initial commit of tAllocator.
    For STL use.
    
    Signed-off-by: Mihail Grigorov <megrigorov@gmail.com>

---
+++ core/include/tAllocator.h
@@ -0,0 +1,73 @@
+#ifndef __TALLOCATOR_H__
+#define __TALLOCATOR_H__
+
+#include "iMemory.h"
+#include "iRepository.h"
+
+template <typename T>
+class tAllocator {
+private:
+	iHeap	*mpi_heap;
+
+	iHeap *get_heap(void) {
+		iHeap *r = mpi_heap;
+
+		if(!r)
+			r = mpi_heap = dynamic_cast<iHeap *>(_gpi_repo_->object_by_iname(I_HEAP, RF_ORIGINAL));
+
+		return r;
+	}
+public:
+	typedef size_t size_type;
+	typedef ptrdiff_t difference_type;
+	typedef T* pointer;
+	typedef const T* const_pointer;
+	typedef const void* const_void_pointer;
+	typedef T& reference;
+	typedef const T& const_reference;
+	typedef T value_type;
+
+	tAllocator(){
+		mpi_heap = NULL;
+	}
+	~tAllocator(){
+		if(mpi_heap)
+			_gpi_repo_->object_release(mpi_heap);
+	}
+
+	template <class U> struct rebind { typedef tAllocator<U> other; };
+	template <class U> tAllocator(const tAllocator<U>&){}
+	pointer address(reference x) const {return &x;}
+	const_pointer address(const_reference x) const {
+		return &x;
+	}
+	size_type max_size() const throw() {
+		return size_t(-1) / sizeof(value_type);
+	}
+	pointer allocate(size_type n, const_void_pointer hint = 0) {
+		pointer r = NULL;
+		iHeap *pi_heap = get_heap();
+
+		if(pi_heap)
+			r = static_cast<pointer>(pi_heap->alloc(n * sizeof(T)));
+
+		return r;
+	}
+	void deallocate(pointer p, size_type n) {
+		iHeap *pi_heap = get_heap();
+
+		if(pi_heap)
+			pi_heap->free(p, n * sizeof(T));
+	}
+	void construct(pointer p, const T& val) {
+		new(static_cast<void*>(p)) T(val);
+	}
+	void construct(pointer p) {
+		new(static_cast<void*>(p)) T();
+	}
+	void destroy(pointer p) {
+		p->~T();
+	}
+};
+
+#endif
