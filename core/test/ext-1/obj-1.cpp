982a3c6894ccc96f51dd8126deee16de833cd910
Mihail Grigorov <megrigorov@gmail.com>
2019-10-10 10:48:55 +0300

    core/test: code update
    Signed-off-by: Mihail Grigorov <megrigorov@gmail.com>

--- core/test/ext-1/obj-1.cpp
+++ core/test/ext-1/obj-1.cpp
@@ -21,9 +21,8 @@ private:
 	}
 
 	BEGIN_LINK_MAP
-		{ BASE_REF(mpi_log),	I_LOG,		NULL,	RF_ORIGINAL,	[](_u32 n, void *udata) {
-			//...
-		}, this },
+		LINK(mpi_log, I_LOG, NULL, RF_ORIGINAL, [](_u32 n, void *udata) {
+		}, this),
 	END_LINK_MAP
 public:
 	BASE(cObj_1, "cObj_1", RF_CLONE|RF_TASK, 1,0,0);
