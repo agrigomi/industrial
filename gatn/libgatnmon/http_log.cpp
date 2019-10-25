e2822c26340d38dfddfb38874d1e448c70ba489b
Mihail Grigorov <megrigorov@gmail.com>
2019-10-25 11:21:10 +0300

    gatn/gatnmon: Adds logging options:
    -i for regular requests (INFO)
    -e for errors.
    -t for text (request content)
    
    Signed-off-by: Mihail Grigorov <megrigorov@gmail.com>

--- gatn/libgatnmon/http_log.cpp
+++ gatn/libgatnmon/http_log.cpp
@@ -18,77 +18,90 @@ private:
 	_http_evt_t	m_original_evt[MAX_EVENTS]; // backup events
 	_server_t	*mpi_gatn_server;
 	_char_t		m_host_name[256];
+	bool		e, i, t;
 
 	void backup_handlers(_server_t *psrv, _cstr_t host) {
-		m_original_evt[ON_REQUEST].p_cb = psrv->get_event_handler(ON_REQUEST, &(m_original_evt[ON_REQUEST].udata), host);
-		m_original_evt[ON_DATA].p_cb = psrv->get_event_handler(ON_DATA, &(m_original_evt[ON_DATA].udata), host);
-		m_original_evt[ON_ERROR].p_cb = psrv->get_event_handler(ON_ERROR, &(m_original_evt[ON_ERROR].udata), host);
+		if(i)
+			m_original_evt[ON_REQUEST].p_cb = psrv->get_event_handler(ON_REQUEST, &(m_original_evt[ON_REQUEST].udata), host);
+		if(t)
+			m_original_evt[ON_DATA].p_cb = psrv->get_event_handler(ON_DATA, &(m_original_evt[ON_DATA].udata), host);
+		if(e)
+			m_original_evt[ON_ERROR].p_cb = psrv->get_event_handler(ON_ERROR, &(m_original_evt[ON_ERROR].udata), host);
 	}
 
 	void set_handlers(_server_t *psrv, _cstr_t host) {
-		psrv->on_event(ON_REQUEST, [](_request_t *req, _response_t *res, void *udata) {
-			cHttpLog *pobj = (cHttpLog *)udata;
-			iLog *pi_log = pobj->mpi_log;
-			_cstr_t method = req->var(VAR_REQ_METHOD);
-			_cstr_t uri = req->var(VAR_REQ_URI);
-			_cstr_t protocol = req->var(VAR_REQ_PROTOCOL);
-			_char_t ip[32];
+		if(i) {
+			psrv->on_event(ON_REQUEST, [](_request_t *req, _response_t *res, void *udata) {
+				cHttpLog *pobj = (cHttpLog *)udata;
+				iLog *pi_log = pobj->mpi_log;
+				_cstr_t method = req->var(VAR_REQ_METHOD);
+				_cstr_t uri = req->var(VAR_REQ_URI);
+				_cstr_t protocol = req->var(VAR_REQ_PROTOCOL);
+				_char_t ip[32];
 
-			req->connection()->peer_ip(ip, sizeof(ip));
+				req->connection()->peer_ip(ip, sizeof(ip));
 
-			pi_log->fwrite(LMT_INFO, "%s/%s: (%s) %s %s %s",
-						pobj->mpi_gatn_server->name(),
-						(strlen(pobj->m_host_name)) ? pobj->m_host_name: "defaulthost",
-						ip,
-						method, protocol, uri);
+				pi_log->fwrite(LMT_INFO, "%s/%s: (%s) %s %s %s",
+							pobj->mpi_gatn_server->name(),
+							(strlen(pobj->m_host_name)) ? pobj->m_host_name: "defaulthost",
+							ip,
+							method, protocol, uri);
 
-			_u32 size = 0;
-			_cstr_t data = (_cstr_t)req->data(&size);
+				_u32 size = 0;
+				_cstr_t data = (_cstr_t)req->data(&size);
 
-			if(data) {
-				pi_log->write(LMT_TEXT, data);
-				req->parse_content();
-			}
+				if(data) {
+					pi_log->write(LMT_TEXT, data);
+					req->parse_content();
+				}
 
-			pobj->call_original_handler(ON_REQUEST, req, res);
-		}, this, host);
+				pobj->call_original_handler(ON_REQUEST, req, res);
+			}, this, host);
+		}
 
-		psrv->on_event(ON_DATA, [](_request_t *req, _response_t *res, void *udata) {
-			cHttpLog *pobj = (cHttpLog *)udata;
-			iLog *pi_log = pobj->mpi_log;
-			_u32 size = 0;
-			_cstr_t data = (_cstr_t)req->data(&size);
+		if(t) {
+			psrv->on_event(ON_DATA, [](_request_t *req, _response_t *res, void *udata) {
+				cHttpLog *pobj = (cHttpLog *)udata;
+				iLog *pi_log = pobj->mpi_log;
+				_u32 size = 0;
+				_cstr_t data = (_cstr_t)req->data(&size);
 
-			if(data)
-				pi_log->write(LMT_TEXT, data);
+				if(data)
+					pi_log->write(LMT_TEXT, data);
 
-			pobj->call_original_handler(ON_DATA, req, res);
-		}, this, host);
+				pobj->call_original_handler(ON_DATA, req, res);
+			}, this, host);
+		}
 
-		psrv->on_event(ON_ERROR, [](_request_t *req, _response_t *res, void *udata) {
-			cHttpLog *pobj = (cHttpLog *)udata;
-			iLog *pi_log = pobj->mpi_log;
-			_cstr_t uri = req->var(VAR_REQ_URI);
-			_char_t ip[32];
-			_u16 rc = res->error();
-			_cstr_t rc_text = res->text(rc);
+		if(e) {
+			psrv->on_event(ON_ERROR, [](_request_t *req, _response_t *res, void *udata) {
+				cHttpLog *pobj = (cHttpLog *)udata;
+				iLog *pi_log = pobj->mpi_log;
+				_cstr_t uri = req->var(VAR_REQ_URI);
+				_char_t ip[32];
+				_u16 rc = res->error();
+				_cstr_t rc_text = res->text(rc);
 
-			req->connection()->peer_ip(ip, sizeof(ip));
+				req->connection()->peer_ip(ip, sizeof(ip));
 
-			pi_log->fwrite(LMT_ERROR, "%s/%s: (%s) ERROR(%d) %s %s",
-						pobj->mpi_gatn_server->name(),
-						(strlen(pobj->m_host_name)) ? pobj->m_host_name: "defaulthost",
-						ip, rc, rc_text,
-						 (uri) ? uri : "");
+				pi_log->fwrite(LMT_ERROR, "%s/%s: (%s) ERROR(%d) %s %s",
+							pobj->mpi_gatn_server->name(),
+							(strlen(pobj->m_host_name)) ? pobj->m_host_name: "defaulthost",
+							ip, rc, rc_text,
+							 (uri) ? uri : "");
 
-			pobj->call_original_handler(ON_ERROR, req, res);
-		}, this, host);
+				pobj->call_original_handler(ON_ERROR, req, res);
+			}, this, host);
+		}
 	}
 
 	void restore_handlers(_server_t *psrv, _cstr_t host) {
-		psrv->on_event(ON_REQUEST, m_original_evt[ON_REQUEST].p_cb, m_original_evt[ON_REQUEST].udata, host);
-		psrv->on_event(ON_DATA, m_original_evt[ON_DATA].p_cb, m_original_evt[ON_DATA].udata, host);
-		psrv->on_event(ON_ERROR, m_original_evt[ON_ERROR].p_cb, m_original_evt[ON_ERROR].udata, host);
+		if(i)
+			psrv->on_event(ON_REQUEST, m_original_evt[ON_REQUEST].p_cb, m_original_evt[ON_REQUEST].udata, host);
+		if(t)
+			psrv->on_event(ON_DATA, m_original_evt[ON_DATA].p_cb, m_original_evt[ON_DATA].udata, host);
+		if(e)
+			psrv->on_event(ON_ERROR, m_original_evt[ON_ERROR].p_cb, m_original_evt[ON_ERROR].udata, host);
 	}
 
 	void call_original_handler(_u8 evt, _request_t *req, _response_t *res) {
@@ -127,8 +140,15 @@ public:
 	bool options(_cstr_t opt) {
 		bool r = false;
 
-		if(opt)
-			r = mpi_args->init(opt, strlen(opt));
+		e = i = t = false;
+
+		if(opt) {
+			if((r = mpi_args->init(opt, strlen(opt)))) {
+				e = mpi_args->check('e');
+				i = mpi_args->check('i');
+				t = mpi_args->check('t');
+			}
+		}
 
 		return r;
 	}
