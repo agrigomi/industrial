5e72891a3f3c2314025573404eb41efb96ff4131
Mihail Grigorov <agrigomi@abv.bg>
2019-12-14 13:42:29 +0200

    db: Interface update (save)
    Signed-off-by: Mihail Grigorov <agrigomi@abv.bg>

--- db/interface/iSQL.h
+++ db/interface/iSQL.h
@@ -1,15 +1,30 @@
 #ifndef __I_SQL_H__
 #define __I_SQL_H__
 
+#define I_SQL		"iSQL"
 #define I_SQL_POOL	"iSQLPool"
 
 #include "iBase.h"
 
-
+typedef struct {
+	//...
+}_sql_t;
 
 class iSQLPool: public iBase {
 public:
 	INTERFACE(iSQLPool, I_SQL_POOL);
+
+	virtual bool init(_cstr_t connection_string)=0;
+	virtual _sql_t *alloc(void)=0;
+	virtual void free(_sql_t *)=0;
+};
+
+class iSQL: public iBase {
+public:
+	INTERFACE(iSQL, I_SQL);
+
+	virtual iSQLPool *create_sql_pool(_cstr_t connection_string)=0;
+	virtual void destroy_sql_pool(iSQLPool *)=0;
 };
 
 #endif
