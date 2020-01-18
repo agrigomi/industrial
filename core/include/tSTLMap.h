699a207ea22c673a3a8ceafc7532bd2c66103908
Mihail Grigorov <agrigomi@abv.bg>
2020-01-18 20:20:16 +0200

    core: Adds more convenient method 'insert' for tSTLMap.
    Signed-off-by: Mihail Grigorov <agrigomi@abv.bg>

--- core/include/tSTLMap.h
+++ core/include/tSTLMap.h
@@ -5,6 +5,11 @@
 #include "tAllocator.h"
 
 template <typename key, typename T>
-class tSTLMap: public std::map<key, T, std::less<key>, tAllocator<T>> {};
+class tSTLMap: public std::map<key, T, std::less<key>, tAllocator<T>> {
+public:
+	void insert(key _k, T _v) {
+		std::map<key, T, std::less<key>, tAllocator<T>>::insert(std::pair<key, T>(_k, _v));
+	}
+};
 
 #endif
