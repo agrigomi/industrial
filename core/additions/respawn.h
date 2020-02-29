5467466bf3d38aa694f108dfc61f98d39f8869ff
Mihail Grigorov <agrigomi@abv.bg>
2020-02-29 20:19:17 +0200

    core/additions/respawn: No changes in code.
    Signed-off-by: Mihail Grigorov <agrigomi@abv.bg>

--- core/additions/respawn.h
+++ core/additions/respawn.h
@@ -21,6 +21,7 @@ typedef struct {
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 /* Execute a file with arguments and environment.
    return 0 for sucess -1 for error */
 int proc_exec_ve(_proc_t *, /* process context */
@@ -28,35 +29,46 @@ int proc_exec_ve(_proc_t *, /* process context */
 		const char *const argv[], /* array of arguments */
 		const char *const envp[] /* array of environment variables */
 		);
+
 /* Execute a file with arguments.
    return 0 for sucess -1 for error */
 int proc_exec_v(_proc_t *, /* process context */
 		const char *path, /* path to executable */
 		const char *const argv[] /* array of arguments */
 		);
+
 /* Read from stdout of a child process.
    return -1 for error, or number of received bytes */
 int proc_read(_proc_t *, void *buf, int sz);
+
 /* Read from stdout of a child process with timeout in seconds.
    return -1 for error, or number of received bytes */
 int proc_read_ts(_proc_t *, void *buf, int sz, int ts);
+
 /* Write to stdin of a child process.
    return -1 for error, or number of number of written bytes */
 int proc_write(_proc_t *, void *buf, int sz);
+
 /* Return status of child process, if available.
    Othrewise -1 will be returned. */
 int proc_status(_proc_t *);
+
 /* wait for process to change state */
 int proc_wait(_proc_t *);
+
 /* Do not forget to call this function !!! */
 void proc_close_pipe(_proc_t *);
+
 /* kill child process.
    return 0 for success, -1 for error */
 int proc_kill(_proc_t *);
+
 /* Send SIGINT to child process.
    return 0 for sucess, -1 for error */
 int proc_break(_proc_t *);
+
 #ifdef __cplusplus
 }
 #endif
+
 #endif
